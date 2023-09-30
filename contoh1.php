a9ae68ab7ec7725fe845b417c9ebc24d548d6dc7
AgentK <145754785+AgentKodok@users.noreply.github.com>
2023-09-30 12:41:27 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,12 @@
+    <?php
+    class contoh1 extends CI_Controller
+    {
+        public function index()
+        {
+            echo "<h1>Perkenalkan</h1>";
+            echo "Nama saya Putra Ibrahim
+                Saya tinggal di daerah London
+                olah raga yang saya sukai adalah
+                memanjat jam big ben";
+        }
+    }
